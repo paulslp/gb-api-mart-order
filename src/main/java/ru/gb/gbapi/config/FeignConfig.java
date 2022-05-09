cb129e5b64fb3bc443cfc9e64ee97b9b6d967dba
PSelyutin <pselyutin@inno.tech>
2022-05-09 11:03:57 +0400

    lesson7 add bean names to config

--- src/main/java/ru/gb/gbapi/config/FeignConfig.java
+++ src/main/java/ru/gb/gbapi/config/FeignConfig.java
@@ -1,32 +1,14 @@
 package ru.gb.gbapi.config;
 
-import feign.*;
-import feign.codec.ErrorDecoder;
-import feign.optionals.OptionalDecoder;
-import feign.slf4j.Slf4jLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
-import org.springframework.cloud.openfeign.support.SpringDecoder;
-import org.springframework.cloud.openfeign.support.SpringEncoder;
-import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.converter.HttpMessageConverter;
 import ru.gb.gbapi.category.api.CategoryGateway;
-import ru.gb.gbapi.category.dto.CategoryDto;
 import ru.gb.gbapi.manufacturer.api.ManufacturerGateway;
 import ru.gb.gbapi.product.api.ProductGateway;
-
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
-import static feign.FeignException.errorStatus;
-import static feign.Util.RETRY_AFTER;
 
 @Configuration
 @EnableFeignClients
@@ -38,19 +20,22 @@ public class FeignConfig {
     private final GbApiProperties gbApiProperties;
     private final FeignClientFactory feignClientFactory;
 
-    @Bean
+    @Bean(name = "categoryGateway")
     public CategoryGateway categoryGateway() {
-        return feignClientFactory.newFeignClient(CategoryGateway.class, gbApiProperties.getEndpoint().getCategoryUrl());
+        return feignClientFactory.newFeignClient(CategoryGateway.class,
+                gbApiProperties.getEndpoint().getCategoryInternalUrl());
     }
 
-    @Bean
+    @Bean(name = "manufacturerGateway")
     public ManufacturerGateway manufacturerGateway() {
-        return feignClientFactory.newFeignClient(ManufacturerGateway.class, gbApiProperties.getEndpoint().getManufacturerUrl());
+        return feignClientFactory.newFeignClient(ManufacturerGateway.class,
+                gbApiProperties.getEndpoint().getManufacturerInternalUrl());
     }
 
-    @Bean
+    @Bean(name = "productGateway")
     public ProductGateway productGateway() {
-        return feignClientFactory.newFeignClient(ProductGateway.class, gbApiProperties.getEndpoint().getProductUrl());
+        return feignClientFactory.newFeignClient(ProductGateway.class,
+                gbApiProperties.getEndpoint().getProductInternalUrl());
     }
 
 }
